48e6e38ca4982ccc4b00d26ea096ff63b2b02e82
Ederson Torresini <etorresini@ifsc.edu.br>
2026-03-24 12:05:03 +0000

    Adicionar botões de inverter gravidade e pular

--- scene0.js
+++ scene0.js
@@ -30,8 +30,12 @@ class scene0 extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32,
     });
+    this.load.spritesheet("buttons", "buttons.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
 
-    this.load.audio("music", "music.mp3");
+    // this.load.audio("music", "music.mp3");
     this.load.audio("laser", "laser.mp3");
 
     this.load.plugin(
@@ -46,45 +50,67 @@ class scene0 extends Phaser.Scene {
 
     this.tilesetTileset = this.tilemap.addTilesetImage("tileset");
     this.tilesetObjects = this.tilemap.addTilesetImage("objects");
+    /*
     this.tilesetTurret = this.tilemap.addTilesetImage("turret");
-    // this.tilesetCharacter = this.tilemap.addTilesetImage("character");
-    // this.tilesetAndroid = this.tilemap.addTilesetImage("android");
+    this.tilesetCharacter = this.tilemap.addTilesetImage("character");
+    this.tilesetAndroid = this.tilemap.addTilesetImage("android");
+    */
 
     this.layerBackground = this.tilemap.createLayer("background", [
       this.tilesetTileset,
     ]);
     this.layerGround = this.tilemap.createLayer("ground", [
       this.tilesetTileset,
+      this.tilesetObjects,
     ]);
-    this.layerRoof = this.tilemap.createLayer("roof", [this.tilesetTileset]);
-    this.layerWalls = this.tilemap.createLayer("walls", [this.tilesetTileset]);
+    this.layerRoof = this.tilemap.createLayer("roof", [
+      this.tilesetTileset,
+      this.tilesetObjects,
+    ]);
+    this.layerWalls = this.tilemap.createLayer("walls", [
+      this.tilesetTileset,
+      this.tilesetObjects,
+    ]);
     this.layerWallsUnder = this.tilemap.createLayer("walls_under", [
       this.tilesetTileset,
+      this.tilesetObjects,
     ]);
     this.layerWallsOver = this.tilemap.createLayer("walls_over", [
       this.tilesetTileset,
+      this.tilesetObjects,
     ]);
-    this.layerLamps = this.tilemap.createLayer("lamps", [this.tilesetTileset]);
+    this.layerLamps = this.tilemap.createLayer("lamps", [
+      this.tilesetTileset,
+      this.tilesetObjects,
+    ]);
     this.layerWindows = this.tilemap.createLayer("windows", [
       this.tilesetTileset,
+      this.tilesetObjects,
     ]);
     this.layerObjects = this.tilemap.createLayer("objects", [
       this.tilesetTileset,
+      this.tilesetObjects,
     ]);
-    // this.layerCharacter = this.tilemap.createLayer("character", [
-    //   this.tilesetCharacter,
-    // ]);
-    // this.layerEnemy = this.tilemap.createLayer("enemy", [this.tilesetAndroid]);
-    this.layerPlatform = this.tilemap.createLayer("platform", [
-      this.tilesetTileset,
-    ]);
-    this.layerShelf = this.tilemap.createLayer("shelf", [this.tilesetTileset]);
-
-    this.player = this.physics.add.sprite(150, 600, "character", 0);
-
     this.layerTeletransport = this.tilemap.createLayer("teletransport", [
       this.tilesetTileset,
+      this.tilesetObjects,
     ]);
+    /*
+    this.layerCharacter = this.tilemap.createLayer("character", [
+      this.tilesetCharacter,
+    ]);
+    this.layerEnemy = this.tilemap.createLayer("enemy", [this.tilesetAndroid]);
+    */
+    this.layerPlatform = this.tilemap.createLayer("platform", [
+      this.tilesetTileset,
+      this.tilesetObjects,
+    ]);
+    this.layerShelf = this.tilemap.createLayer("shelf", [
+      this.tilesetTileset,
+      this.tilesetObjects,
+    ]);
+
+    this.player = this.physics.add.sprite(150, 656, "character", 0);
 
     this.anims.create({
       key: "standing-still",
@@ -154,12 +180,12 @@ class scene0 extends Phaser.Scene {
     this.physics.add.collider(this.player, this.layerPlatform);
     this.layerPlatform.forEachTile((tile) => {
       if (tile.properties.collides) {
-        //left, right, up, down
+        // left, right, up, down
         tile.setCollision(false, false, true, false);
       }
     });
 
-    this.music = this.sound.add("music", { loop: true }).play();
+    // this.music = this.sound.add("music", { loop: true }).play();
     this.laser = this.sound.add("laser");
 
     this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, {
@@ -183,18 +209,11 @@ class scene0 extends Phaser.Scene {
 
       if (this.joystick.force > 0) {
         switch (true) {
-          // jump
-          case this.joystick.angle >= -110 &&
-            this.joystick.angle < -50 &&
-            this.player.body.blocked.down:
-            this.player.setVelocityY(-150);
-            this.player.anims.play("jumping", true);
-            break;
           // right
           case this.joystick.angle >= -20 && this.joystick.angle < 20:
             this.player.flipX = false;
             this.player.setVelocityX(200);
-            if (this.player.body.blocked.down) {
+            if (this.player.body.blocked.down || this.player.body.blocked.up) {
               this.player.anims.play("running", true);
             }
             break;
@@ -202,7 +221,7 @@ class scene0 extends Phaser.Scene {
           case this.joystick.angle >= 160 || this.joystick.angle < -160:
             this.player.flipX = true;
             this.player.setVelocityX(-200);
-            if (this.player.body.blocked.down) {
+            if (this.player.body.blocked.down || this.player.body.blocked.up) {
               this.player.anims.play("running", true);
             }
             break;
@@ -211,16 +230,54 @@ class scene0 extends Phaser.Scene {
         this.player.setVelocityX(0);
       }
     });
+
+    this.changeGravityButton = this.add
+      .sprite(700, 400, "buttons", 0)
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.changeGravityButton.setFrame(1);
+        this.physics.world.gravity.y *= -1;
+        this.player.setFlipY(this.physics.world.gravity.y < 0);
+      })
+      .on("pointerup", () => {
+        this.changeGravityButton.setFrame(0);
+      })
+      .setScrollFactor(0);
+
+    this.jumpButton = this.add
+      .sprite(750, 400, "buttons", 8)
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.jumpButton.setFrame(9);
+        this.jump(this.player, this.physics.world.gravity.y);
+      })
+      .on("pointerup", () => {
+        this.jumpButton.setFrame(8);
+      })
+      .setScrollFactor(0);
   }
 
   update() {
     if (
       this.player.body.velocity.x === 0 &&
       this.player.body.velocity.y === 0 &&
-      this.player.body.blocked.down
+      (this.player.body.blocked.down || this.player.body.blocked.up)
     ) {
       this.player.anims.play("standing-still", true);
     }
+  }
+
+  jump(player, gravity) {
+    if (gravity > 0) {
+      if (player.body.blocked.down) {
+        player.setVelocityY(-150);
+      }
+    } else {
+      if (player.body.blocked.up) {
+        player.setVelocityY(150);
+      }
+    }
+    player.anims.play("jumping", true);
   }
 }
 
